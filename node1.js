494e81bb8a236dbd707a308cc97bafb5e8248a07
Hammad498 <hammadabrar498@gmail.com>
2025-04-02 20:56:59 +0500

    first commit

---
+++ node1.js
@@ -0,0 +1,25 @@
+const fs=require('fs');
+const {fs11}=require('./node2.js');
+console.log(fs11(2,2));
+fs.writeFileSync("node1.txt","hello",(err)=>{
+    if(err) throw err;
+    console.log("File created successfully");
+})
+
+
+fs.readFileSync("node1.txt","utf-8",(err,data)=>{
+    if(data){
+        console.log(data);
+    }
+    else{
+        console.log(err);
+    }
+})
+
+fs.appendFileSync("node1.txt", new Date().toString(), (err) => {
+    if (err) throw err;
+    console.log("Data appended successfully");    
+})
+
+
+
---
+++ node2.js
@@ -0,0 +1,10 @@
+
+
+
+function fs1(a,a){
+    return a+a;
+}
+
+module.exports = {
+    fs11: fs1
+}
