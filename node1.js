37fe0b3c4d674ef830530607bdde6ddd9b61021c
Hammad498 <hammadabrar498@gmail.com>
2025-04-02 21:40:44 +0500

    setTimeOut/setInterval

--- node1.js
+++ node1.js
@@ -49,3 +49,19 @@ server.listen(3000,()=>{
     console.log("Server is running on port 3000");
 })
 
+/////////////////////////
+
+setTimeout(()=>{
+    console.log("hello world")
+},2000)
+
+
+let count=0;
+
+const cc=setInterval(()=>{
+    console.log(`${count}:the count is ${count}`);
+    if(count===5){
+        clearInterval(cc);
+    }
+    count++;
+},1000)
