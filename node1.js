6563ce130f0b0f6e74396c85bf4c5f8a84428ea4
Hammad498 <hammadabrar498@gmail.com>
2025-04-07 23:03:38 +0500

    day4

--- node1.js
+++ node1.js
@@ -74,11 +74,82 @@ const cc=setInterval(()=>{
 //on(eventname,Listener)....create
 //emit(eventname,[args])....execuute
 
-const emitter=require('events');
-const eventEmitter=new emitter();
+// const emitter=require('events');
+// const eventEmitter=new emitter();
 
-eventEmitter.on('e1', () => {
-    console.log("Event 1 triggered");
-});
+// const path=require('path');
 
-eventEmitter.emit('e1');
+
+// eventEmitter.on('e1', (args) => {
+//     console.log(`Event 1 triggered : ${args.username}`);
+// });
+
+// eventEmitter.emit('e1',{
+//     username:"john",
+//     id:1
+// });
+
+// //////////////////////
+// const eventcounts={
+//     LOGIN:0,
+//     LOGOUT:0,
+//     PURCHASE:0,
+//     UPDATEPROFILE:0
+// }
+
+// const logfile='log.json';    
+
+// if(fs.existsSync(logfile)){
+//     const data=fs.readFileSync(logfile,'utf-8');
+//    Object.assign(eventcounts,JSON.parse(data));
+// }
+
+
+// function savecounts(){
+//     fs.writeFileSync(logfile,JSON.stringify(eventcounts,null,2))
+// }
+
+// eventEmitter.on('LOGIN',(username)=>{
+//     console.log("User logged in successfully",username);
+//     eventcounts.LOGIN++;
+//     console.log(eventcounts);
+//     savecounts();
+    
+// })
+
+// eventEmitter.on('LOGOUT',(username)=>{
+//     console.log("User logged out successfully",username);
+//     eventcounts.LOGOUT++;
+//     console.log(eventcounts);
+//     savecounts();
+
+// })
+
+// eventEmitter.on('PURCHASE',(username,id)=>{
+//     console.log("User pharsed successfully",username,id);
+//     eventcounts.PURCHASE++;
+//     console.log(eventcounts);
+//     savecounts();
+// })
+
+// eventEmitter.on('UPDATEPROFILE',(username,field)=>{
+//     console.log("User update profile successfully",username,field);
+//     eventcounts.UPDATEPROFILE++;
+//     console.log(eventcounts);
+//     savecounts();
+// })
+
+// eventEmitter.on('summary',()=>{
+//     console.log("\n event summary:");
+//     console.log(eventcounts.LOGIN,"login events");
+//     console.log(eventcounts.LOGOUT,"logout events");    
+//     console.log(eventcounts.PURCHASE,"purchase events");
+//     console.log(eventcounts.UPDATEPROFILE,"update profile events");
+//     console.log("total events",eventcounts.LOGIN+eventcounts.LOGOUT+eventcounts.PURCHASE+eventcounts.UPDATEPROFILE);
+// })
+
+// eventEmitter.emit('LOGIN',"john");
+// eventEmitter.emit('LOGOUT',"john");
+// eventEmitter.emit('PURCHASE',"john",1);
+// eventEmitter.emit('UPDATEPROFILE',"john","email");
+// eventEmitter.emit('summary');
