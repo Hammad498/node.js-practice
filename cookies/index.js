38335266c21af8c49f2f2255240788206a06a571
Hammad498 <hammadabrar498@gmail.com>
2025-04-12 07:41:41 +0500

    cookie_middlewware & signedCookies

--- cookies/index.js
+++ cookies/index.js
@@ -1,24 +1,39 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
-const app=express();
-const PORT=4000
+// 2. cookie-parser as middleware
+const app = express();
 
-app.get("/",(req,res)=>{
-    res.cookie("name","express",{
-        maxAge:1000*60*60*24,
+//3.signed cookie
+app.use(cookieParser("secret"));
+const PORT = 4000;
+
+app.get("/", (req, res) => {
+    // 1. cookies set and expire
+    res.cookie("name", "express", {
+        maxAge: 1000 * 60 * 60 * 24,
+        signed:true,
     });
     res.send("hello");
-})
+});
+
+app.get('/products', (req, res) => {
+    // Corrected from req.cookie to req.cookies
+    if (req.cookies.name && req.cookies.name === 'express') {
+        console.log(req.cookies);
+        console.log(req.signedCookies);
 
 
-app.get('/products',(req,res)=>{
-    res.status(200).send({
-        id:1,
-        name:"tel",
-        price:4000
-    })
-})
+        res.status(200).send({
+            id: 1,
+            name: "tel",
+            price: 4000
+        });
+    } else {
+        res.status(404).send(`you are not authorized!`);
+    }
+});
 
-app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+});
