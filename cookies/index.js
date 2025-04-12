0ba6779f3fb1ffdaa2079c6bedcdf1e57d2f7584
Hammad498 <hammadabrar498@gmail.com>
2025-04-12 07:17:28 +0500

    cookie creation and expire

--- cookies/index.js
+++ cookies/index.js
@@ -4,7 +4,19 @@ const app=express();
 const PORT=4000
 
 app.get("/",(req,res)=>{
+    res.cookie("name","express",{
+        maxAge:1000*60*60*24,
+    });
     res.send("hello");
+})
+
+
+app.get('/products',(req,res)=>{
+    res.status(200).send({
+        id:1,
+        name:"tel",
+        price:4000
+    })
 })
 
 app.listen(PORT,()=>{
