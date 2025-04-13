1742c995820eb2e0d6ca8ecbd5f56135cfa59264
Hammad498 <hammadabrar498@gmail.com>
2025-04-13 07:53:18 +0500

    ..

--- project_2/index.js
+++ project_2/index.js
@@ -1,6 +1,21 @@
 import express from "express"
+import cookieParser from "cookie-parser";
+import session from "express-session";
+
+
 
 const app=express();
+
+app.use(session({
+    secret:"hello",
+    saveUninitialized:false,
+    resave:false,
+    cookie:{
+        maxAge:100*60*60*24,
+    }
+}));
+
+app.use(cookieParser("hello2"));
 
 app.get("/",(req,res)=>{
     res.send('hello..');
