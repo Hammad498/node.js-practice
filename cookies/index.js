2cbb556216fd3a306e6b9be6a373b7a0816a327c
Hammad498 <hammadabrar498@gmail.com>
2025-04-13 07:40:40 +0500

    sessions creation,configuration,append user & destroy

--- cookies/index.js
+++ cookies/index.js
@@ -1,8 +1,23 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import session from "express-session";
 
 // 2. cookie-parser as middleware
 const app = express();
+
+
+//4.sessions--> as middleware , configuration
+app.use(session({
+    secret:"mysecret",
+    saveUninitialized:false,
+    resave:false,
+    cookie:{
+        maxAge:1000*60*60*24,
+    }
+}));
+
+
+app.use(cookieParser("node.js"));
 
 //3.signed cookie
 // app.use(cookieParser("secret"));
@@ -14,6 +29,10 @@ app.get("/", (req, res) => {
     //     maxAge: 1000 * 60 * 60 * 24,
     //     signed:true,
     // });
+
+
+    console.log(req.session);
+    console.log(req.session.id);
     res.send("hello");
 });
 
@@ -33,6 +52,20 @@ app.get("/", (req, res) => {
 //         res.status(404).send(`you are not authorized!`);
 //     }
 // });
+
+
+//5.session  ----> append user
+app.get("/login",(req,res)=>{
+    req.session.user={
+        id:1,
+        name:"john",
+        age:7
+    }
+
+    res.send('user loged in!',req.session.user.name);
+})
+
+//6.req.session.destroy()                e.g: loged out and cookie also deleted
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
