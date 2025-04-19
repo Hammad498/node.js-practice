6c8e49776e27e6c9e5c232ba018b68356a921c58
Hammad498 <hammadabrar498@gmail.com>
2025-04-19 17:06:29 +0500

    mongodb

--- project_2/middleware/auth.middleware.js
+++ project_2/middleware/auth.middleware.js
@@ -1,5 +1,5 @@
 ///weather session exists or not and user exists or not   (before logout i think so)---->checking through middleware
-/////////
+
 export const Middleware=(req,res,next)=>{
 
     if(req.session && req.session.user){
