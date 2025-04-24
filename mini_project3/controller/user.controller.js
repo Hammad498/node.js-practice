af52313d56e6d0312fdc7e4194c493d892d92011
Hammad498 <hammadabrar498@gmail.com>
2025-04-24 20:54:02 +0500

    updated done

--- mini_project3/controller/user.controller.js
+++ mini_project3/controller/user.controller.js
@@ -40,10 +40,36 @@ export const createUsers=async(req,res)=>{
 }
 
 
-export const updateUsers=async(req,res)=>{
-    res.send('user update')
-}
+export const updateUsers = async (req, res) => {
+    const { id } = req.params;
+    const { name, age, weight } = req.body;
 
+    try {
+        // const parsedId = parseInt(id);
+
+        // if (!id || isNaN(parsedId)) {
+        //     return res.status(401).json({
+        //         message: "id is required."
+        //     });
+        // }
+
+        const updateUser  = await User.findByIdAndUpdate(id, { name, age, weight }, { new: true, runValidators: true });
+
+        if (!updateUser ) {
+            return res.status(404).json({
+                message: "User  not found."
+            });
+        }
+
+        res.status(200).json({
+            message: "User  updated successfully",
+            updateUser 
+        });
+
+    } catch (error) {
+        res.status(500).json({ error: "Internal server error: updateUsers", error });
+    }
+}
 export const deleteUsers=async(req,res)=>{
     res.send('user delete')
 }
