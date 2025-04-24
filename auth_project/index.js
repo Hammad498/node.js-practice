774a7b99ea34d83702cfefa6a88c4dd443275bd2
Hammad498 <hammadabrar498@gmail.com>
2025-04-24 22:08:21 +0500

    connected successfully! db

--- auth_project/index.js
+++ auth_project/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
 
 dotenv.config();
@@ -7,6 +8,8 @@ dotenv.config();
 
 const app=express();
 const PORT=4000;
+app.use(express.json());
+connectDB();
 
 
 
