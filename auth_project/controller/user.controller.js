093f3ac1c039e337fe98299996e6bcd45a862d3d
Hammad498 <hammadabrar498@gmail.com>
2025-04-26 13:43:08 +0500

    service.js (register user)

--- auth_project/controller/user.controller.js
+++ auth_project/controller/user.controller.js
@@ -1,9 +1,22 @@
 
 
-
+import User from "../models/user.model.js";
+import { registerUser } from "../services/user.service.js";
 
 export const signup=async(req,res)=>{
-    res.status(200).json({message:"Hello from signup route"});
+    const {username,password}=req.body;
+
+
+    try {
+        const user=await registerUser(username,password);
+        res.status(201).json({message:"user created successfully!",success:true,data:user});
+        
+    } catch (error) {
+        res.status(500).json({error:"Internal server error:signup",error});
+        
+    }
+
+    
 }
 
 
