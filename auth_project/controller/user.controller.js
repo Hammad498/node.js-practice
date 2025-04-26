40b38eb5ab37c3f766aa057551f1a8be36de4aea
Hammad498 <hammadabrar498@gmail.com>
2025-04-26 13:27:46 +0500

    user route

---
+++ auth_project/controller/user.controller.js
@@ -0,0 +1,18 @@
+
+
+
+
+export const signup=async(req,res)=>{
+    res.status(200).json({message:"Hello from signup route"});
+}
+
+
+export const login=async(req,res)=>{
+    res.status(200).json({message:"Hello from login route"});
+}
+
+
+
+export const logout=async(req,res)=>{
+    res.status(200).json({message:"Hello from logout route"});
+}
