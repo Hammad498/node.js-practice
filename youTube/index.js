1fbe1f1e84d8ba8d686acc1b92ad58ec04d48ce8
Hammad498 <hammadabrar498@gmail.com>
2025-04-29 19:10:59 +0500

    new project new energy

---
+++ youTube/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+
+
+dotenv.config();
+
+const app=express();
+
+
+
+app.get("/",(req,res)=>{
+    res.send("Home!")
+})
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on:${PORT}`)
+})
