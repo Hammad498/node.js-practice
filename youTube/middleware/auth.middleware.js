ee59d8688c6dde7f9f33cf2dc46c2c544cf6c0b5
Hammad498 <hammadabrar498@gmail.com>
2025-05-02 12:48:10 +0500

    checkAuth

--- youTube/middleware/auth.middleware.js
+++ youTube/middleware/auth.middleware.js
@@ -4,8 +4,22 @@ import jwt from 'jsonwebtoken'
 export const checkAuth=async(req,res,next)=>{
 
     try {
-        
+
+        //login user has a token (get that token), then verify that token then he can upload video
+        const token=req.headers.authorization?.split(" ")[1];
+
+        if(!token){
+            return res.status(401).json({
+                message:'no token provided!'
+            })
+        }
+        const decodedUser=jwt.verify(token,process.env.JWT_TOKEN);
+        req.user=decodedUser;
+        next();
+
+
     } catch (error) {
-        
+        console.error("authentication error:", error);
+      res.status(500).json({ error: "Internal server error: checkAuth", error });
     }
 }
