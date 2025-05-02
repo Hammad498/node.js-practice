b163c3cae857dfefc6f3757be107346ab4dd2dc9
Hammad498 <hammadabrar498@gmail.com>
2025-05-02 12:23:02 +0500

    video schema

---
+++ youTube/models/video.model.js
@@ -0,0 +1,81 @@
+import mongoose, { Schema } from "mongoose";
+import User from "./user.model.js";
+
+
+const videoSchema=new Schema({
+    _id:{
+        type:mongoose.Types.ObjectId,
+    },
+    title:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    description:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    user_id:{
+        type:mongoose.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    videoUrl:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    thumbnailUrl:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    thumbnailId:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    category:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    tags:[{
+        type:String,
+        trim:true
+    }],
+    likes:{
+        type:Number,
+        default:0,
+        min:0
+    },
+    dislikes:{
+        type:Number,
+        default:0,
+        min:0
+    },
+    views:{
+        type:Number,
+        default:0,
+        min:0
+    },
+    likedBy:{
+        type:mongoose.Types.ObjectId,
+        ref:"User"
+    },
+    dislikedBy:{
+        type:mongoose.Types.ObjectId,
+        ref:"User"
+    },
+    viewedBy:{
+        type:mongoose.Types.ObjectId,
+        ref:"User"
+    },
+
+},{timestamps:true});
+
+
+const videoModel=mongoose.model("Video",videoSchema);
+
+export default videoModel;
