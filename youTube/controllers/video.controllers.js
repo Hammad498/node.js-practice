5c50e4892b59492f6ed33c9722e88bb166151a3b
Hammad498 <hammadabrar498@gmail.com>
2025-05-02 14:55:08 +0500

    after errors finally upload worked

--- youTube/controllers/video.controllers.js
+++ youTube/controllers/video.controllers.js
@@ -1,10 +1,47 @@
-
+import mongoose from "mongoose";
+import cloudinary from "../config/cloudinary.js";
+import Video from "../models/video.model.js";
 
 //only authenticated user upload the video   (middleware to check it)
 export const upload=async(req,res)=>{
     try {
         
+
+        // console.log("FILES RECEIVED:", req.files);
+        // console.log("BODY RECEIVED:", req.body);
+
+        if(!req.files || !req.files.videoUrl || !req.files.thumbnailUrl){
+            return  res.status(400).json({
+                error:"Video and thumbnail are required!"
+            })
+        }
+        
+        const videoUpload=await cloudinary.uploader.upload(req.files.videoUrl.tempFilePath,{resource_type:"video",folder:"videos"});
+        const thumbnailUpload=await cloudinary.uploader.upload(req.files.thumbnailUrl.tempFilePath,{folder:"thumbnails"});
+
+        const newVideo=new Video({
+            _id:new mongoose.Types.ObjectId(),
+            title:req.body.title,
+            description:req.body.description,
+            user_id:req.user._id,
+            videoUrl:videoUpload.secure_url,
+            videoId:videoUpload.public_id,
+            thumbnailUrl:thumbnailUpload.secure_url,
+            thumbnailId:thumbnailUpload.public_id,
+            category:req.body.category,
+            tags:req.body.tags? req.body.tags.split(','):[],
+
+        })
+
+        let video=await newVideo.save();
+
+        res.status(200).json({
+            message:"Uploaded successfully!",
+            video
+        })
+        
     } catch (error) {
+        console.error("UPLOAD ERROR:", error);
         res.status(500).json({
             message:"Error (failed to upload)",
             error
--- youTube/index.js
+++ youTube/index.js
@@ -16,7 +16,7 @@ dotenv.config();
 const app=express();
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 app.use(fileUpload({
     useTempFiles:true,
     tempFileDir:"/tmp/"
