dcd71b51bada6d61f14ea7f924c4ea9297b9a84e
Hammad498 <hammadabrar498@gmail.com>
2025-05-12 23:52:01 +0500

    edit successsfully

--- youTube/controllers/comments.controllers.js
+++ youTube/controllers/comments.controllers.js
@@ -74,3 +74,45 @@ export const deleteComment=async(req,res)=>{
         })
     }
 }
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+export const editComment = async (req, res) => {
+    try {
+        const { commentId } = req.params;
+        const { commentText } = req.body;
+
+        const comment = await Comment.findById(commentId);
+
+        if (!comment) {
+            return res.status(404).json({
+                message: "Not found that comment!"
+            });
+        }
+
+        if (comment.user_id.toString() !== req.user._id.toString()) {
+            return res.status(403).json({
+                message: "Forbidden!:unauthorized"
+            });
+        }
+
+        comment.commentText = commentText;
+        await comment.save();
+
+        res.status(200).json({
+            message: "Successfully updated comment!",
+            data: comment
+        });
+
+        console.log(comment);
+
+    } catch (error) {
+        console.error("EDIT COMMENT ERROR:", error);
+        res.status(500).json({
+            message: "Something went wrong!:edit_comment",
+            error: error.message
+        });
+    }
+};
--- youTube/routes/comments.routes.js
+++ youTube/routes/comments.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import {checkAuth} from '../middleware/auth.middleware.js'
-import {comment,deleteComment} from '../controllers/comments.controllers.js'
+import {comment,deleteComment,editComment} from '../controllers/comments.controllers.js'
 
 const router=Router();
 
 router.post('/new',checkAuth,comment);
 router.delete('/delete/:id',checkAuth,deleteComment)
+router.put('/:commentId',checkAuth,editComment)
 
 
 export default router;
